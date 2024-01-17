9c2ae0bac30c8e8e59fc0f7d3fba39b03b8698bf
Pierre Gastellu <pierre.gastellu97@orange.fr>
2024-01-17 11:05:52 +0100

    ExerciceCSV_1701

--- phone_book.py
+++ phone_book.py
@@ -1,1 +1,18 @@
+import csv
 
+#url='https://wagon-public-datasets.s3.amazonaws.com/01-Python/02-Data-Sourcing/phone_book.csv > data/phone_book.csv'
+
+"""with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        print(f"{row[1]}: {row[2]}" )
+        line_count += 1"""
+        
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file, skipinitialspace=True)
+    #line_count = 0
+    for row in csv_reader:
+        print(row['last_name'] +': '+ row['phone_number'] )
+        #line_count += 1
+#print(type(row))
